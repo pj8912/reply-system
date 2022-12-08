6e2a4107afea7d030f2924dff581a3c6537312ce
pj8912 <jpintofun2b@gmail.com>
2022-12-08 20:32:06 +0530

    added comments model

--- src/Models/CommentModel.php
+++ src/Models/CommentModel.php
@@ -9,7 +9,34 @@ class CommentModel{
 		$this->conn = $db;
 	}
 	
-	private $table;
+	private $table="comments";
+
+	public $cid, $pid, $body;
+
+	public function createComment(){
+
+		$sql = "INSERT INTO {$this->table}(pid, body) VALUES(:pid, :body)";
+		$stmt = $this->conn->prepare($sql);
+		$stmt->bindParam(':pid', $this->pid, PDO::PARAM_INT);
+		$stmt->bindParam(':body', $this->body, PDO::PARAM_STR);
+		if($stmt->execute()){
+			return true;
+		}
+		return false;
+
+	}
+
+	public function fetchComments(){
+
+		$sql = "SELECT * FROM {$this->table}";
+		$stmt = $this->conn->prepare($sql);
+		if($stmt->execute()){
+			return $stmt->execute();
+		}
+		return false;
+	}
+
+
 
 
 }
